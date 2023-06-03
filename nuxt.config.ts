52b3bc563f9a137abf91cfa1dde58406644d03f7
StefanGhiban <stefanghiban962@gmail.com>
2023-06-03 14:12:59 +0300

    added devtools and eslint modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,10 +1,21 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-    modules: ["@sidebase/nuxt-auth", "@nuxtjs/tailwindcss"],
+    modules: [
+        "@sidebase/nuxt-auth",
+        "@nuxtjs/tailwindcss",
+        "@nuxtjs/eslint-module",
+        "@nuxt/devtools",
+    ],
+
     build: {
         transpile: ["trpc-nuxt"],
     },
+
     typescript: {
         shim: false,
+    },
+
+    devtools: {
+        enabled: true,
     },
 });
