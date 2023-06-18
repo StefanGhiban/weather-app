e0068e6d4de966c0e6b09167d19a9d59040943e6
StefanGhiban <stefanghiban962@gmail.com>
2023-06-18 18:41:57 +0300

    added inter font

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -19,4 +19,15 @@ export default defineNuxtConfig({
     devtools: {
         enabled: true,
     },
+
+    app: {
+        head: {
+            link: [
+                {
+                    rel: "stylesheet",
+                    href: "https://fonts.googleapis.com/css2?family=Inter:wght@100;200;300;400;500;600;700;800;900&display=swap",
+                },
+            ],
+        },
+    },
 });
