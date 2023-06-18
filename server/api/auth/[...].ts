795a1ef0ea103271079a20d46db0607693f7b7d8
StefanGhiban <stefanghiban962@gmail.com>
2023-06-18 18:24:25 +0300

    solved typescript error

--- server/api/auth/[...].ts
+++ server/api/auth/[...].ts
@@ -2,12 +2,13 @@ import GithubProvider from "next-auth/providers/github";
 import DiscordProvider from "next-auth/providers/discord";
 import { PrismaAdapter } from "@auth/prisma-adapter";
 import { PrismaClient } from "@prisma/client";
+import { Adapter } from "next-auth/adapters";
 import { NuxtAuthHandler } from "#auth";
 
 const prisma = new PrismaClient();
 
 export default NuxtAuthHandler({
-    adapter: PrismaAdapter(prisma),
+    adapter: PrismaAdapter(prisma) as Adapter,
     // TODO: ADD YOUR OWN AUTHENTICATION PROVIDER HERE, READ THE DOCS FOR MORE: https://sidebase.io/nuxt-auth
     providers: [
         // @ts-expect-error You need to use .default here for it to work during SSR. May be fixed via Vite at some point
